a44ef7a35c0689d6700b27770506513a54d023f7
Yohan Alek <yh9dev@gmail.com>
2023-11-16 12:32:48 -0700

    Se terminó el diseño de la página create.php

--- public/products/create.php
+++ public/products/create.php
@@ -1,3 +1,35 @@
 <?php include_once "../../views/partials/header.php"; ?>
-
+    <section class="products-section">
+        <div class="products-content container">
+            <form action="create.php" method="post">
+                <h2>Añade un nuevo producto</h2>
+                <div class="mb-3">
+                    <label for="formFileSm" class="form-label">Sube una imagen para tu producto</label>
+                    <input class="form-control form-control-sm" id="formFileSm" type="file">
+                </div>
+                <div class="mb-3">
+                    <label for="title" class="form-label">Título</label>
+                    <input name="title" type="text" class="form-control" id="title" placeholder="Título de ejemplo">
+                </div>
+                <div class="mb-3">
+                    <label for="description" class="form-label">Descripción</label>
+                    <textarea class="form-control" id="description" rows="3" placeholder="Este es un producto 👍"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="price" class="form-label">Precio</label>
+                    <input name="price" type="number" class="form-control" id="price" min="0" value="0">
+                </div>
+                <div class="mb-3">
+                    <label for="rate" class="form-label">Calificación</label>
+                    <div class="rate-container">
+                        <div class="stars">
+                            <i class="fa-solid fa-star"></i>
+                            <input name="rate" type="number" class="form-control" id="rate" min="0" max="5" value="1">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Agregar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
 <?php include_once "../../views/partials/footer.php";?>
--- public/products/index.php
+++ public/products/index.php
@@ -1,17 +1,27 @@
+<?php
+/** @var $pdo PDO */
+require_once "../../database.php";
+
+$statement = $pdo->prepare('SELECT * FROM products_crud_2 ORDER BY rate DESC;');
+$statement->execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+?>
 <?php include_once "../../views/partials/header.php"; ?>
     <section class="products-section">
         <div class="products-content container">
             <div class="swiper products-swiper">
                 <div class="swiper-wrapper">
+                    <?php foreach ($products as $product): ?>
                     <div class="swiper-slide">
                         <div class="card" style="width: 18rem;">
-                            <img src="https://cdn.discordapp.com/attachments/1129195909796860029/1167308512024608879/watch.jpg?ex=65601cc4&is=654da7c4&hm=ab6fb7bfa30cf181844979eab6eabfd7890c3571d7f57e571cb48eef162b628e&" class="card-img-top" alt="...">
+                            <img src="<?= $product['image'] ?>" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Description of the product.</p>
+                                <h5 class="card-title"><?= $product['title'] ?></h5>
+                                <p class="card-text"><?= $product['description'] ?></p>
                                 <div class="price-container">
                                     <a href="#" class="btn btn-primary">Comprar</a>
-                                    <h4>$15000</h4>
+                                    <h4><?= number_format($product['price'], 2, '.', ',') ?></h4>
                                 </div>
                                 <div class="stars">
                                     <i class="fa-solid fa-star"></i>
@@ -23,89 +33,8 @@
                             </div>
                         </div>
                     </div>
-                    <div class="swiper-slide">
-                        <div class="card" style="width: 18rem;">
-                            <img src="https://cdn.discordapp.com/attachments/1129195909796860029/1167308512024608879/watch.jpg?ex=65601cc4&is=654da7c4&hm=ab6fb7bfa30cf181844979eab6eabfd7890c3571d7f57e571cb48eef162b628e&" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Description of the product.</p>
-                                <div class="price-container">
-                                    <a href="#" class="btn btn-primary">Comprar</a>
-                                    <h4>$15000</h4>
-                                </div>
-                                <div class="stars">
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="card" style="width: 18rem;">
-                            <img src="https://cdn.discordapp.com/attachments/1129195909796860029/1167308512024608879/watch.jpg?ex=65601cc4&is=654da7c4&hm=ab6fb7bfa30cf181844979eab6eabfd7890c3571d7f57e571cb48eef162b628e&" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Description of the product.</p>
-                                <div class="price-container">
-                                    <a href="#" class="btn btn-primary">Comprar</a>
-                                    <h4>$15000</h4>
-                                </div>
-                                <div class="stars">
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="card" style="width: 18rem;">
-                            <img src="https://cdn.discordapp.com/attachments/1129195909796860029/1167308512024608879/watch.jpg?ex=65601cc4&is=654da7c4&hm=ab6fb7bfa30cf181844979eab6eabfd7890c3571d7f57e571cb48eef162b628e&" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Description of the product.</p>
-                                <div class="price-container">
-                                    <a href="#" class="btn btn-primary">Comprar</a>
-                                    <h4>$15000</h4>
-                                </div>
-                                <div class="stars">
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="card" style="width: 18rem;">
-                            <img src="https://cdn.discordapp.com/attachments/1129195909796860029/1167308512024608879/watch.jpg?ex=65601cc4&is=654da7c4&hm=ab6fb7bfa30cf181844979eab6eabfd7890c3571d7f57e571cb48eef162b628e&" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Description of the product.</p>
-                                <div class="price-container">
-                                    <a href="#" class="btn btn-primary">Comprar</a>
-                                    <h4>$15000</h4>
-                                </div>
-                                <div class="stars">
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <?php endforeach; ?>
                 </div>
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
             </div>
         </div>
     </section>
